629983aae29a9227e782fc80a9609f54da3d445a
Ansh Goyal <94157520+anshgoyalevil@users.noreply.github.com>
2022-07-10 15:14:39 +0530

    Time: 21 ms (6.05%), Space: 40.7 MB (63.82%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,24 +1,11 @@
 class Solution {
-    
-    public int f(int n, int [] dp){
-        
-        if(dp[n]!=0){
-            return dp[n];
-        }
+    public int fib(int n) {
         
         if(n == 0 || n == 1){
             return n;
         }
         
-        return dp[n] = fib(n-1) + fib(n-2);
-        
-    }
-    
-    public int fib(int n) {
-        
-        int [] dp = new int[n+1];
-        
-        return f(n, dp);
+        return fib(n-1) + fib(n-2);
         
     }
 }
