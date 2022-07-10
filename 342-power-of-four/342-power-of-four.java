bdad6429876cc488f1904adefd6671665a216f0b
Ansh Goyal <94157520+anshgoyalevil@users.noreply.github.com>
2022-07-10 15:15:39 +0530

    Time: 2 ms (63.24%), Space: 41.4 MB (47.06%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        
+        if(n == 1){
+            return true;
+        }
+        
+        else if(n%4!=0 || n == 0){
+            return false;
+        }
+        
+        return isPowerOfFour(n/4);
+        
+    }
+}
