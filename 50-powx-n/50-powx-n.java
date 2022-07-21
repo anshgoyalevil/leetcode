327889c472cda0bdebca1346ecb4a1502dfde2cb
Ansh Goyal <94157520+anshgoyalevil@users.noreply.github.com>
2022-07-21 07:12:51 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (83.61%) - LeetHub

---
+++ 50-powx-n/50-powx-n.java
@@ -0,0 +1,37 @@
+class Solution {
+    public double my(double x, int n) {
+        
+        if(n == 0){
+            return 1;
+        }
+        
+        double a = myPow(x, n/2);
+        double b = a*a;
+        
+        if(n%2==1){
+            b = b*x;
+        }
+        
+        return b;
+        
+    }
+    
+    public double myPow(double x, int n) {
+        
+        if(n==0){
+            return (double)1;
+        }
+        else if(n<0){
+            x = 1d/x;
+            int n1 = (n/2);
+            int n2 = (n - n1);
+            n1 = -n1;
+            n2 = -n2;
+            return my(x, n1)*my(x, n2);
+        }
+        
+        return my(x, n);
+        
+    }
+    
+}
