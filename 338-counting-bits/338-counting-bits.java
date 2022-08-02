f0c1df19513bf4740d7b0b5ef8b175c46fe75d0c
Ansh Goyal <94157520+anshgoyalevil@users.noreply.github.com>
2022-08-02 10:35:29 +0530

    Time: 2 ms (88.08%), Space: 48.7 MB (11.56%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int [] ans = new int [n+1];
+        
+        ans[0] = 0;
+        
+        for(int i = 1; i<=n; i++){
+            ans[i] = ans[i/2] + i%2;
+        }
+        
+        return ans;
+        
+    }
+}
