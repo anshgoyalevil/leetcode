4bba7dbbc518fa44f2f9cfbbe95bbe8a6bbd8d48
Ansh Goyal <anshgoyal1704@gmail.com>
2022-08-26 09:26:51 +0530

    Time: 35 ms (5.46%), Space: 44.4 MB (6.21%) - LeetHub

---
+++ 67-add-binary/67-add-binary.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    public String addBinary(String a, String b) {
+        int rem = 0;
+
+        String ans = "";
+
+        int a1 = 0;
+        int a2 = 0;
+
+        int r1 = a.length() - 1;
+        int r2 = b.length() - 1;
+
+        while (r1 >= 0 || r2 >= 0) {
+            if (r1 < 0 && r2 >= 0) {
+                a1 = 0;
+                a2 = b.charAt(r2) - 48;
+            } else if (r2 < 0 && r1 >= 0) {
+                a2 = 0;
+                a1 = a.charAt(r1) - 48;
+            } else {
+                a1 = a.charAt(r1) - 48;
+                a2 = b.charAt(r2) - 48;
+            }
+
+            
+
+            ans = ((char) (((a1 + a2 + rem) % 2) + 48)) + ans;
+            
+            rem = (a1 + a2 + rem) / 2;
+            
+            System.out.println(ans);
+
+            r1--;
+            r2--;
+        }
+        if(rem!=0){
+            ans = '1'+ans;
+        }
+        return ans;
+    }
+}
