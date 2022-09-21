a6dffbb72554d096f1739062d22c5215671471f6
Ansh Goyal <anshgoyal1704@gmail.com>
2022-09-21 19:33:57 +0530

    Time: 43 ms (34.70%), Space: 69.3 MB (63.35%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.java
@@ -1,36 +1,21 @@
 class Solution {
     public int minSteps(String s, String t) {
         
-        HashMap<Character, Integer> h1 = new HashMap<>();
-        HashMap<Character, Integer> h2 = new HashMap<>();
-        
         int ans = 0;
         
+        int [] a1 = new int [26];
+        int [] a2 = new int [26];
+        
         for(int i = 0; i<s.length(); i++){
-            h1.put(s.charAt(i), h1.getOrDefault(s.charAt(i), 0)+1);
+            a1[s.charAt(i)-97]++;
         }
         
         for(int i = 0; i<t.length(); i++){
-            h2.put(t.charAt(i), h2.getOrDefault(t.charAt(i), 0)+1);
+            a2[t.charAt(i)-97]++;
         }
         
-        for(Map.Entry<Character, Integer> e: h1.entrySet()){
-            if(h2.containsKey(e.getKey())){
-                ans+=Math.abs(h1.get(e.getKey())-h2.get(e.getKey()));
-                h2.remove(e.getKey());
-            }
-            else{
-                ans+=e.getValue();
-            }
-        }
-        
-        for(Map.Entry<Character, Integer> e: h2.entrySet()){
-            if(h1.containsKey(e.getKey())){
-                ans+=Math.abs(h1.get(e.getKey())-h2.get(e.getKey()));
-            }
-            else{
-                ans+=e.getValue();
-            }
+        for(int i = 0; i<26; i++){
+            ans+=Math.abs(a1[i]-a2[i]);
         }
         return ans;
     }
