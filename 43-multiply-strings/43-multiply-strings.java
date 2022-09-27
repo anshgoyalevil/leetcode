e2882da19e60f85abdfb1d1617154a585fe914ce
Ansh Goyal <anshgoyal1704@gmail.com>
2022-09-27 18:43:22 +0530

    Time: 9 ms (59.28%), Space: 42.5 MB (85.59%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public String multiply(String num1, String num2) {
+        int m = num1.length();
+        int n = num2.length();
+        int ans[] = new int[m + n]; //max possible size of prod
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int prod = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int temp = ans[i + j + 1] + prod; // to store carry
+                ans[i + j] += temp / 10;
+                ans[i + j + 1] = temp % 10;
+            }
+        }
+        StringBuilder st = new StringBuilder();
+        for (int i : ans) {
+            if (
+                i != 0 || st.length() != 0
+            ) st.append(i); //avoid leading zeros
+        }
+        if (st.length() == 0) return "0"; // product 0
+        return st.toString();
+    }
+}
