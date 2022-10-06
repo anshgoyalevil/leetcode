761f471c9eeb05684fc4ddc705b1c7dd8d0c4427
Ansh Goyal <anshgoyal1704@gmail.com>
2022-10-06 22:31:16 +0530

    Time: 0 ms (100.00%), Space: 54.2 MB (51.64%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] arr, int x) {
+        
+        int l = 0, r = arr.length - 1;
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+ 
+            if (arr[m] == x)
+                return m;
+ 
+            if (arr[m] < x)
+                l = m + 1;
+ 
+            else
+                r = m - 1;
+        }
+ 
+        return -1;
+        
+    }
+}
