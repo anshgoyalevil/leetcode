d7ffb92b8ac4cd05fc06abfb8f4660ce06500b4a
Ansh Goyal <anshgoyal1704@gmail.com>
2022-10-14 22:35:45 +0530

    Time: 100 ms (5.14%), Space: 71.1 MB (6.53%) - LeetHub

---
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumGroups(int[] g) {
+        
+        int ans = 1;
+        
+        Arrays.sort(g);
+        
+        int ps = 0;
+        int pg = 0;
+        int sf = 0;
+        int gf = 0;
+        
+        for(int i = 0; i<g.length; i++){
+            if(sf<=ps || gf<=pg){
+                sf+=g[i];
+                gf++;
+            }
+            else{
+                System.out.println(ps + " " + pg + " " + sf + " " + gf);
+                ans++;
+                ps = sf;
+                pg = gf;
+                sf = g[i];
+                gf = 1;
+            }
+        }
+        
+        if(sf<=ps || gf<=pg){
+            ans--;
+        }
+        
+        return ans;
+    }
+}
