01dbc85292b007b26cdee48db7e50733528d49cc
Ansh Goyal <anshgoyal1704@gmail.com>
2022-12-22 13:15:52 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (81.98%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,24 @@
+class Solution {
+    
+    public void f(boolean [] r, int id, List<List<Integer>> rooms){
+        
+        r[id] = true;
+        
+        for(int e: rooms.get(id)){
+            if(r[e] == false){
+                f(r, e, rooms);
+            }
+        }
+    }
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] r = new boolean[rooms.size()];
+        f(r, 0, rooms);
+        for(boolean b: r){
+            if(!b){
+                return b;
+            }
+        }
+        return true;
+    }
+}
